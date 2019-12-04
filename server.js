d5ee6d96696896b916a25649f384b303eba82aa1
AustinHadden <vitovalletori@gmail.com>
2019-12-04 12:12:13 -0600

    guided project activity

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// custom middleware
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl}`);
+  next(); // allows the request to continue to the next middleware or route handler
+}
+
+// write a gatekeeper middleware that reads a password from the headers and if the password is 'mellon' let it continue
+// if not send back status code 401 and a message
+
+function gatekeeper(req, res, next) {
+  const password = req.headers.password;
+  if(password === 'mellon'){
+    next();
+  } else {
+    res.status(401).json({ message: "Incorrect password."})
+  }
+}
+
+
+server.use(helmet());
 server.use(express.json());
+server.use(logger)
 
 server.use('/api/hubs', hubsRouter);
 
@@ -15,6 +37,10 @@ server.get('/', (req, res) => {
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
+});
+
+server.get("/area51", gatekeeper(), (req, res) => {
+  res.send(req.headers);
 });
 
 module.exports = server;
